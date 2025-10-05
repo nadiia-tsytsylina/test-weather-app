0baac15a355c9bca1a101b9cb38254273c2e707b
Nadiia Tsytsylina <miniova95@gmail.com>
2025-10-05 16:38:12 +0300

    feat: Install dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
-    plugins: ["prettier"],
+
+    plugins: {
+      prettier: prettierPlugin,
+    },
+
     rules: {
-      "prettier/prettier": [
-        "error",
+      'prettier/prettier': [
+        'error',
         {
           singleQuote: true,
           semi: true,
-          trailingComma: "all",
-          endOfLine: "auto",
+          trailingComma: 'all',
+          endOfLine: 'auto',
         },
       ],
     },
